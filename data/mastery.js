bf79df9bc6b8cd8ae6d4986629941473dc0b2a3b
alfgeir <alfgeir>
2026-05-29 13:58:37 +0000

    first commit

---
+++ data/mastery.js
@@ -0,0 +1,13 @@
+var masteryTag = {
+tagSkillName001:"Warfare",
+tagSkillName018:"Storm",
+tagSkillName030:"Spirit",
+tagSkillName050:"Rogue",
+tagSkillName065:"Nature",
+tagSkillName076:"Hunting",
+tagSkillName098:"Earth",
+tagSkillName116:"Defense",
+x2tagSkillRunesName001:"Rune",
+x4tagNeidanMastery:"Neidan",
+xtagSkillDreamName001:"Dream",
+};
